6736255302fd46116ed9033276c7a7136a2bd0b9
Bronson Brown <Brsn90@icloud.com>
2019-11-13 16:30:04 -0700

    Add files via upload

---
+++ components/FunctionalComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const FunctionalComponent = () => {
+   return( 
+       <div>This is Functional Component</div>
+   )
+};
+
+export const AnotherComponent = () => {
+    return(
+       <div>This is another Component</div> 
+    )
+};
+export default FunctionalComponent;
